b42ce97e4f5a9c21094206234046bee5d68d71d9
kzncod-e <kznrehan@gmail.com>
2024-09-27 17:53:19 +0700

    updated

--- blogger/src/views/Home.jsx
+++ blogger/src/views/Home.jsx
@@ -1,9 +1,8 @@
-// import Login from "./Login";
 import { useEffect, useState } from "react";
 
 import axios from "axios";
 import Card from "../components/Cards";
-// const url = "https://h8-phase2-gc.vercel.app";
+
 export default function Home({ url }) {
   const [posts, setPosts] = useState([]);
 
